6c69696184ff9c27d4d2ae2652c5d5db23970a73
Xendrra <47497023+Xendrra@users.noreply.github.com>
2019-03-12 23:32:37 +0100

    Update VigenerCipher.R

--- VigenerCipher.R
+++ VigenerCipher.R
@@ -1,10 +1,12 @@
 VigenerCipher<-function(text,klic){ 
   #funkce VigenerCipher vraci sifrovany text
-  #vstup1...text, retezec ktery ma byt sifrovan
-  #vstup2...klic, retezec pouzity k zasifrovani
+  #vstup1...text, retezec ktery ma byt sifrovan, pouze znaky anglicke alfabety
+  #vstup2...klic, retezec pouzity k zasifrovani, pouze znaky anglicke alfabety
   #vystup...sifra, zakodovany vstupni text
   text=gsub(" ", "", text, fixed = TRUE)        #odstraneni mezer
   klic=gsub(" ", "", klic, fixed = TRUE)  
+  text=tolower(text)                            #osetreni v pripade kapitalek na vstupu
+  klic=tolower(klic)
   a=letters                                     #pomocny vektor s pismeny
   j=1 
   temp=numeric()                                #iniciace promennych
@@ -14,8 +16,9 @@ VigenerCipher<-function(text,klic){
     temp[i]= which(a==substring(klic,j,j))-1    #docasna promenna, prevadi pismena na cisla posunu (a=0)
     out[i]=which(a==substring(text,i,i))+temp[i] #najde index kazdeho znaku v pomocnem vektoru a pricte zadany posun 
     if (out[i]>26){                             #pokud index prekroci index pismene z, zacina znovu od a
-      out[i]=out[i]-26                         
-    } 
+      out[i]=out[i]-26 
+    }else if(out[i]>52){out[i]=out[i]-52}
+    
     if (j==nchar(klic)){                        #pokud index j presahne delku klice, vraci se zpet k prvnimu znaku
       j=1
     }else                                       #jinak pokracuje na dalsi znak klice
